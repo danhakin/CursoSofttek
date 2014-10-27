a7a78b45e502ee46540883bf8559d603bb96f77c
Pablo Formoso Estrada <pablo@pabloformoso.com>
2014-10-27 20:39:27 +0100

    Deberes

--- GestionDeFicheros/GestionDeFicheros/SWAppDelegate.h
+++ GestionDeFicheros/GestionDeFicheros/SWAppDelegate.h
@@ -1,4 +1,12 @@
-
+/*
+ Crear un nuevo UIViewController en el StoryBoard
+ Crear un nuevo fichero que extienda de UIViewController
+ Asociarlo al agreagado en el StoryBoard
+ 
+ Incluir un UIImageView en el ViewController creado
+ Crear el IBOutlet
+ Al nagevar con el segue MODAL enviar el UIImage para ponerlo en el componente del otro contralador
+ */
 
 #import <UIKit/UIKit.h>
 
